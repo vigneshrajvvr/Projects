ac451d7c8f52c6951bf1cdcb6e97bea4d805aaf7
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2024-10-04 15:23:05 +0530

    Refactoring

---
+++ Handling_Media_Files_using_Amazon_S3/managemedia/src/main/java/com/practice/vvr/managemedia/ManagemediaApplication.java
@@ -0,0 +1,13 @@
+package com.practice.vvr.managemedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagemediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagemediaApplication.class, args);
+	}
+
+}
