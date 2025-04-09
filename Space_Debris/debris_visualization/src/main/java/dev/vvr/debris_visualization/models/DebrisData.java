b086b7874570034f4813efa68060bde30ae6e39b
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2025-04-09 22:45:05 +0530

    Temporary fix for lombok annotation fix

--- Space_Debris/debris_visualization/src/main/java/dev/vvr/debris_visualization/models/DebrisData.java
+++ Space_Debris/debris_visualization/src/main/java/dev/vvr/debris_visualization/models/DebrisData.java
@@ -7,12 +7,48 @@ import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
-@Getter
-@Setter
-@AllArgsConstructor
 public class DebrisData {
     private String id;
     private String epoch;
     private double inclination;
     private double raan;
+
+    public DebrisData(String id, String epoch, double inclination, double raan) {
+        this.id = id;
+        this.epoch = epoch;
+        this.inclination = inclination;
+        this.raan = raan;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEpoch() {
+        return epoch;
+    }
+
+    public void setEpoch(String epoch) {
+        this.epoch = epoch;
+    }
+
+    public double getInclination() {
+        return inclination;
+    }
+
+    public void setInclination(double inclination) {
+        this.inclination = inclination;
+    }
+
+    public double getRaan() {
+        return raan;
+    }
+
+    public void setRaan(double raan) {
+        this.raan = raan;
+    }
 }
